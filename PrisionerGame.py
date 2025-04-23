f5621b11e3b11151f6e9abb69f334001e061503b
ERyukSa <jslkap227@gmail.com>
2025-04-23 17:18:32 +0900

    Create PrisionerGame.py

---
+++ PrisionerGame.py
@@ -0,0 +1,10 @@
+class PrisionersGame:
+    """docstring for PrisionersGame"""
+    def __init__(self, num_drawers):
+        assert num_drawers % 2 == 0
+        self.num_drawers = num_drawers
+        self.max_attempts = int(self.num_drawers / 2)
+        self.drawer_ids = list(range(1, num_drawers + 1))
+        shuffled = self.drawer_ids[:]
+        random.shuffle(shuffled)
+        self.drawers = dict(zip(self.drawer_ids, shuffled))
